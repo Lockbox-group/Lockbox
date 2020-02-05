6752e07d502fddbc921bed46ca36e77e6ee6f1a0
Your Name <you@example.com>
2020-02-05 15:21:54 -0800

    2/5/2020 UI Initially Finished

---
+++ app/src/main/java/com/example/lotlockbox/customerHistory.java
@@ -0,0 +1,14 @@
+package com.example.lotlockbox;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class customerHistory extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_history);
+
+    }
+}
--- app/src/main/java/com/example/lotlockbox/ui/login/Cus_LoggedInUserView.java
+++ app/src/main/java/com/example/lotlockbox/ui/login/Cus_LoggedInUserView.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lotlockbox.R;
 import com.example.lotlockbox.pickupResult;
+import com.example.lotlockbox.customerHistory;
+import com.example.lotlockbox.settingPage;
 
 /**
  * Class exposing authenticated user details to the UI.
@@ -20,10 +22,26 @@ public class Cus_LoggedInUserView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.customer_loggedin_page);
         final Button PickUpPackageButton = findViewById(R.id.btn_pickup_package);
+        final Button cusHistoryButton = findViewById(R.id.btn_customer_view_history);
+        final Button settingButton = findViewById(R.id.btn_setting);
         PickUpPackageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Cus_LoggedInUserView.this,pickupResult.class);
+                startActivity(intent);
+            }
+        });
+        cusHistoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Cus_LoggedInUserView.this,customerHistory.class);
+                startActivity(intent);
+            }
+        });
+        settingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Cus_LoggedInUserView.this,settingPage.class);
                 startActivity(intent);
             }
         });
